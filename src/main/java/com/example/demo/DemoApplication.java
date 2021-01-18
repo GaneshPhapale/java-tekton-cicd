c95b0d60ee3937dd3d4b143e67d4c48d3eceee26
Ganesh Phapale <ganeshphapale77@gmail.com>
2021-01-18 19:37:41 +0530

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -12,7 +12,7 @@ public class DemoApplication {
 
     @RequestMapping("/")
     String home() {
-        return "Hello World!";
+        return "Hello From Ganesh !";
     }
 
     public static void main(String[] args) {
